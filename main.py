973a704212b743c5686df100b0dea0f390b8676d
icarus <ellishogan95@gmail.com>
2019-11-14 23:10:42 -0500

    update

--- main.py
+++ main.py
@@ -29,5 +29,6 @@ def get_weather():
 	except:
 		return redirect(url_for('index'))
 
+port = int(os.environ.get('PORT', 5000)) 
 if __name__ == '__main__':
-	app.run(threaded=True)
+	app.run(threaded=True, port=port)
