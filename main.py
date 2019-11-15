23d894ee14ea46eaa4aa023d4421edcf2962b634
icarus <ellishogan95@gmail.com>
2019-11-14 20:55:22 -0500

    first

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for, request, redirect
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('weather.html', weather=None, temp=None)
+
+@app.route('/<city>/<country>', methods=['GET', 'POST'])
+@app.route('/<city>/<state>/<country>', methods=['GET', 'POST'])
+def weather(city, country, state=None):
+	if state:
+		req = requests.get(f'https://openweathermap.org/data/2.5/weather?q={city},{state},{country}&units=imperial&appid=b6907d289e10d714a6e88b30761fae22').json()
+	else:
+		req = requests.get(f'https://openweathermap.org/data/2.5/weather?q={city},{country}&appid=b6907d289e10d714a6e88b30761fae22').json()
+	weather = req['weather'][0]
+	temp = req['main']
+	print(weather, temp)
+	return render_template('weather.html', weather=weather, temp=temp)
+
+@app.route('/get_weather', methods=['POST'])
+def get_weather():
+	try:
+		city = request.form['city']
+		state = request.form['state'] if request.form['state'] != "" else None
+		country = request.form['country']
+		return redirect(url_for('weather', city=city, country=country, state=state))
+	except:
+		return redirect(url_for('index'))
+
+if __name__ == '__main__':
+	app.run(debug=True)
