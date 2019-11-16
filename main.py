832c022a69de1c9b69b74e65e9de2d8236b43a0c
icarus <ellishogan95@gmail.com>
2019-11-16 17:30:33 -0500

    all

--- main.py
+++ main.py
@@ -6,27 +6,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('weather.html', weather=None, temp=None)
+	error = request.args.get('error')
+	return render_template('weather.html', weather=None, error=error)
 
-@app.route('/<city>/<country>', methods=['GET', 'POST'])
-@app.route('/<city>/<state>/<country>', methods=['GET', 'POST'])
-def weather(city, country, state=None):
+@app.route('/weather', methods=['GET', 'POST'])
+def weather():
+	city = request.args.get('city')
+	state = request.args.get('state')
+	country = request.args.get('country')
+	unit = request.args.get('unit')
 	if state:
-		req = requests.get(f'https://openweathermap.org/data/2.5/weather?q={city},{state},{country}&units=imperial&appid=b6907d289e10d714a6e88b30761fae22').json()
+		req = requests.get(f'https://openweathermap.org/data/2.5/weather?q={city},{state},{country}&units={unit}&appid=b6907d289e10d714a6e88b30761fae22').json()
 	else:
-		req = requests.get(f'https://openweathermap.org/data/2.5/weather?q={city},{country}&units=imperial&appid=b6907d289e10d714a6e88b30761fae22').json()
+		req = requests.get(f'https://openweathermap.org/data/2.5/weather?q={city},{country}&units={unit}&appid=b6907d289e10d714a6e88b30761fae22').json()
 	weather = req['weather'][0]
 	temp = req['main']
-	print(weather, temp)
-	return render_template('weather.html', weather=weather, temp=temp)
+	return render_template('weather.html', weather=weather, temp=temp, unit=unit, error=None)
 
 @app.route('/get_weather', methods=['POST'])
 def get_weather():
 	try:
 		city = request.form['city']
-		state = request.form['state'] if request.form['state'] != "" else None
+		state = request.form['state']
 		country = request.form['country']
-		return redirect(url_for('weather', city=city, country=country, state=state))
+		unit = request.form['unit-type']
+		print(city, state)
+		if city is "" or country is "":
+			return redirect(url_for('index', error="You need a city and country."))
+		else:
+			return redirect(url_for('weather', city=city, country=country, state=state, unit=unit))
 	except:
 		return redirect(url_for('index'))
 
